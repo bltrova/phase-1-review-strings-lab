7a5d6038ff3571cc4dc2507c2fbee1e6a8f1c3dd
Bruno Trova <bltrova@gmail.com>
2025-01-27 11:38:48 -0800

    review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Bruno Trova';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = ' WELCOME TO FLATBOOK, ' + 'BRUNO TROVA' + '!';
+const shortGreeting = 'Welcome, ' + 'B' + '!';
